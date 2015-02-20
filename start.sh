5b72bea50455147208fb354f79f69b95b3b63cb3
sam@hatoum.net <sam@hatoum.net>
2015-02-19 22:22:38 -0800

    renabling all testing modes

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-export JASMINE_BROWSER=PhantomJS
+#export JASMINE_BROWSER=PhantomJS
 
 export JASMINE_CLIENT_UNIT=1
 export JASMINE_CLIENT_INTEGRATION=1
-export JASMINE_SERVER_UNIT=0
-export JASMINE_SERVER_INTEGRATION=0
+export JASMINE_SERVER_UNIT=1
+export JASMINE_SERVER_INTEGRATION=1
 
 meteor --settings settings.json
