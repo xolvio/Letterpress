26cd2038bbfec187349e908a06d6495a2e9ddc71
Sam Hatoum <sam@xolv.io>
2015-07-11 14:21:38 -0700

    fixes layout test

--- tests/jasmine/client/integration/layout-spec.js
+++ tests/jasmine/client/integration/layout-spec.js
@@ -2,15 +2,13 @@ describe('The layout', function () {
 
   beforeEach(function (done) {
 
-    spyOn(UI._globalHelpers, 'classes');
+    spyOn(UI._globalHelpers, 'classes').and.callThrough();
 
     Meteor.call('fixtures/seedData', function () {
       Router.go('/chapter-1');
       Tracker.afterFlush(done);
     });
   });
-
-  beforeEach(waitForRouter);
 
   fit('should set the classes of the template and path', function () {
 
@@ -24,10 +22,8 @@ describe('The layout', function () {
     expect(UI._globalHelpers.classes).toHaveBeenCalled();
     expect(UI._globalHelpers.classes.calls.count()).toEqual(1);
 
-    // FIXME not working in the test, works manually. Seems like the 'classes' global helper is not called when the route is changed in beforeEach
-    //var templateAndPageClasses = '.chapter.chapter-1'; // set in in the seedData
-    //console.log('current path', Iron.Location.get().path);
-    //expect($(templateAndPageClasses).length).toBe(1);
+    var templateAndPageClasses = '.chapter.chapter-1'; // set in in the seedData
+    expect($(templateAndPageClasses).length).toBe(1);
   });
 
 });
---
+++ tests/jasmine/client/integration/support/before-each-common.js
@@ -0,0 +1,43 @@
+beforeAll(function () {
+  var self = this;
+
+  self.deferAfterFlush = function (callback) {
+    Tracker.afterFlush(function () {
+      Meteor.defer(callback);
+    });
+  };
+
+  // Go into our blank testing sandbox
+  self.initTestingSandbox = function (done) {
+    Router.go('testingSandbox', {}, {query: {jasmine: 'true'}});
+    waitForRouter(function () {
+      // Don't change routes while testing in the sandbox
+      self.routerGoSpy = spyOn(Router, 'go');
+      self.deferAfterFlush(done);
+    });
+  };
+
+  self.waitForElement = function (selector, successCallback) {
+    var checkInterval = 50;
+    var timeoutInterval = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    var startTime = Date.now();
+    var intervalId = Meteor.setInterval(function () {
+      if (Date.now() > startTime + timeoutInterval) {
+        Meteor.clearInterval(intervalId);
+        // Jasmine will handle the test timeout error
+      } else if ($(selector).length > 0) {
+        Meteor.clearInterval(intervalId);
+        successCallback();
+      }
+    }, checkInterval);
+  }
+
+});
+
+// Wait until iron:router has done its initial work
+beforeEach(waitForRouter);
+
+// Guarantee that tests don't run in a ongoing flush cycle.
+beforeEach(function (done) {
+  this.deferAfterFlush(done);
+});
