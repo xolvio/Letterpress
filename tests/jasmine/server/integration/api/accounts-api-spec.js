5ac243bdc748bf5deeeffbbd5596d64168e35bae
Sam Hatoum <sam@xolv.io>
2015-07-18 02:52:36 -0700

    forgot to commit the test for the account-api

---
+++ tests/jasmine/server/integration/api/accounts-api-spec.js
@@ -0,0 +1,22 @@
+describe('Accounts API', function () {
+
+  describe('isSubscribed', function () {
+
+    it('gets the currently signed in user and checks id they are subscribed', function () {
+
+      // SETUP
+      var aUser = {a: 'user'};
+      spyOn(Meteor.users, 'findOne').and.returnValue(aUser);
+      spyOn(Letterpress.Services.Account, 'isSubscribed');
+
+      // EXECUTE
+      getMethod('isSubscribed').apply({userId: 'myId'});
+
+      // VERIFY
+      expect(Meteor.users.findOne).toHaveBeenCalledWith('myId');
+      expect(Letterpress.Services.Account.isSubscribed).toHaveBeenCalledWith(aUser);
+
+    });
+  });
+
+});
