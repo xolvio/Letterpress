ded6f975a5d9baa8c5d9ac992d062d0fd476c600
Sam Hatoum <sam@xolv.io>
2015-07-18 23:51:05 -0700

    allowing package testing to run in parallel with the main app + testing

--- packages/aws-cloudfront-sign/test.sh
+++ packages/aws-cloudfront-sign/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export JASMINE_CLIENT_INTEGRATION=0
 export VELOCITY_TEST_PACKAGES=1
-meteor test-packages --driver-package velocity:html-reporter ./
+meteor --port 5000 test-packages --driver-package velocity:html-reporter ./
