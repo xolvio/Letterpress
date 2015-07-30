6e1ab744522c4130990d8396d4159a649dd97de7
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-07-30 20:17:04 +0200

    Fixes run script

--- run.sh
+++ run.sh
@@ -9,9 +9,13 @@ export JASMINE_SERVER_UNIT=0
 export JASMINE_CLIENT_INTEGRATION=1
 export JASMINE_SERVER_INTEGRATION=1
 export CUCUMBER=1
+export CHIMP_PATH=/home/jonas/projects/chimp/bin/chimp
+#export CHIMP_OPTIONS="--sync=false --debug --log=verbose"
+export VELOCITY_DEBUG=1
 
 if [ "$1" = "--test" ]; then
   export CUCUMBER_TAIL=1;
 fi
 
-meteor $1 --settings settings.json --release velocity:METEOR@1.1.0.2_3 --raw-logs
+cd app
+meteor $1 --settings ../environments/local/settings.json --release velocity:METEOR@1.1.0.2_3 --raw-logs
